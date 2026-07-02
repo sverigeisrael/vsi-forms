d435e0c70aa47ae1e50e9c963a166d0b85246608
VSI Admin <admin@sverigeisrael.se>
2026-07-02 15:52:01 +0200

    Skicka bekräftelse till redaktionen när formuläret skickas in
    Andra EmailJS-sändning till nyhetsbrev@sverigeisrael.se vid inskickning
    (kräver bekräftelse-mall EMAILJS_CONFIRM_TEMPLATE_ID i config.js).

--- config.js
+++ config.js
@@ -6,6 +6,9 @@
 const EMAILJS_PUBLIC_KEY  = 'wZmpyDsQjzlNFK-u1';
 const EMAILJS_SERVICE_ID  = 'service_ngqkjn8';
 const EMAILJS_TEMPLATE_ID = 'template_cbw0eoi';
+// Bekräftelse-mall för nyhetsbrevsformuläret (To Email = nyhetsbrev@sverigeisrael.se).
+// Skapas i EmailJS-dashboarden; klistra in mall-ID:t här. Tomt = ingen bekräftelse skickas.
+const EMAILJS_CONFIRM_TEMPLATE_ID = '';
 
 // Cloudinary (fil-uppladdning)
 const CLOUDINARY_CLOUD_NAME    = 'dh2npg32u';
